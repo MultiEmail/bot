0279cd88f59363dd815b32b658637a761e4910ab
DaatUserName <hopetobylol@gmail.com>
2022-10-03 13:50:10 +0200

    help command

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
 import { join } from 'path';
+import 'dotenv/config';
 
 export const rootDir = join(__dirname, '..', '..');
 export const srcDir = join(rootDir, 'src');
 
 export const RandomLoadingMessage = ['Computing...', 'Thinking...', 'Cooking some food', 'Give me a moment', 'Loading...'];
+
+export const prefix = process.env.prefix;
